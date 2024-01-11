1597e1baf7db634a4d0be7fed5209205f2b717aa
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-11 17:39:57 +0530

    add todoValidator

--- backend/validators/validators.ts
+++ backend/validators/validators.ts
@@ -4,3 +4,8 @@ export const userValidator = z.object ({
     username: z.string().min(1).max(25),
     password: z.string().min(4).max(15)
   })
+
+export const todoValidator = z.object ({
+    title: z.string().min(1).max(30),
+    description: z.string().min(1).max(100)
+  })
