9b83134101a4f27d7078a119f9cb653513e3a642
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-11 17:38:24 +0530

    modify userValidator

--- backend/validators/validators.ts
+++ backend/validators/validators.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const userValidator = z.object ({
-    username: z.string().min(1),
-    password: z.string().min(1)
+    username: z.string().min(1).max(25),
+    password: z.string().min(4).max(15)
   })
