915dafd331994a78bed104b648ef1d805f8bebf0
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-14 22:23:51 +0530

    infer type of signupParams to use in frontend

--- backend/validators/validators.ts
+++ backend/validators/validators.ts
@@ -3,9 +3,11 @@ import { z } from "zod";
 export const userValidator = z.object ({
     username: z.string().min(1).max(25),
     password: z.string().min(4).max(15)
-  })
+  });
 
 export const todoValidator = z.object ({
     title: z.string().min(1).max(30),
     description: z.string().min(1).max(100)
-  })
+  });
+
+export type SignupParams = z.infer<typeof userValidator>;
