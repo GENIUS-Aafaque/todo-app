2d259227089b5a18b3934f52231edc7bce48d84d
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-14 23:45:52 +0530

    create declaration file

---
+++ common/dist/index.d.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+export declare const userValidator: z.ZodObject<{
+    username: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    username: string;
+    password: string;
+}, {
+    username: string;
+    password: string;
+}>;
+export declare const todoValidator: z.ZodObject<{
+    title: z.ZodString;
+    description: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    description: string;
+}, {
+    title: string;
+    description: string;
+}>;
+export type SignupParams = z.infer<typeof userValidator>;
